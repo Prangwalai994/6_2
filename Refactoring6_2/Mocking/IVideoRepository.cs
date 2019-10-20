ba0798d110b59d1520c47d1e3fae9f77e4954876
Prangwalai994 <prangwalai.li@rmuti.ac.th>
2019-10-20 15:15:59 +0700

    6_2 Modify 3

---
+++ Refactoring6_2/Mocking/IVideoRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace Refactoring6_2.Mocking
+{
+    public interface IVideoRepository
+    {
+        System.Collections.Generic.IEnumerable<Video> GetUnprocessedVideos();
+    }
+}
--- Refactoring6_2/Mocking/VideoRepository.cs
+++ Refactoring6_2/Mocking/VideoRepository.cs
@@ -3,18 +3,23 @@ using System.Collections.Generic;
 
 namespace Refactoring6_2.Mocking
 {
-    public class VideoRepository
+    public interface IVideoRepository
+    {
+        IEnumerable<video> GetUnprocessedVideos();
+    }
+    public class VideoRepository : IVideoRepository
     {
         public IEnumerable<Video> GetUnprocessedVideos()
         {
             using (var context = new VideoContext())
             {
-                var  videos =
+                var videos =
                 (from video in context.Videos
                  where !video.IsProcessed
                  select video).ToList();
 
                 return videos;
             }
+        }
     }
 }
