e3e6900555423a5efa3625acb45e91299a994eb7
Prangwalai994 <prangwalai.li@rmuti.ac.th>
2019-10-20 15:18:41 +0700

    6_2 Modify 4

--- Refactoring6_2/Mocking/VideoService.cs
+++ Refactoring6_2/Mocking/VideoService.cs
@@ -15,7 +15,7 @@ namespace TestNinja.Mocking
         public VideoService(IFileReader fileReader = null, IVideoRepository repository = null)
         {
             _fileReader = fileReader ?? new FileReader();
-            _repository = repository ?? new VideoRepository();
+            
         }
 
         public VideoService()
